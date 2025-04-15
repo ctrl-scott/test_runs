6e6d19b94055f81f9cfcfeead2615a62301f73fd
scott owen <oscott66@outlook.com>
2025-04-15 12:14:19 -0500

    file checksum checker with ref
    Found a simple method on a website to check a file checksum using md5 and saving for ref

---
+++ filechecksum_chkr.py
@@ -0,0 +1,22 @@
+#reference used which turned out to be the simplest way just for testing purposes
+#https://bobbyhadz.com/blog/python-calculate-md5-hash-checksum-of-file
+
+import hashlib
+
+
+file_name1 = 'file1.txt'
+file_name2 = 'file2.txt'
+
+with open(file_name1, 'rb') as file_obj:
+    file_contents = file_obj.read()
+
+    md5_hash = hashlib.md5(file_contents).hexdigest()
+
+    print(md5_hash + 'file1')
+
+with open(file_name2, 'rb') as file_obj:
+    file_contents = file_obj.read()
+
+    md5_hash = hashlib.md5(file_contents).hexdigest()
+
+    print(md5_hash + 'file2')
